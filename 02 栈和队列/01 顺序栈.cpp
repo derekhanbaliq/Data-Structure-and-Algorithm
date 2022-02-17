9cdaa45f47e6f674c145e1910a9eee234034fee2
Derek Zhou <derekzhou1105@gmail.com>
2022-02-17 10:38:46 -0500

    Stack

---
+++ 02 栈和队列/01 顺序栈.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+//general define
+#define TRUE			1
+#define FALSE			0
+#define OK				1
+#define ERROR			0
+
+typedef int Status;
+typedef int SElemType;
+
+#define MAXSIZE 100
+
+typedef struct
+{
+	SElemType* base;	//栈底指针
+	SElemType* top;		//栈顶指针
+	int stacksize;		//栈可用最大容量
+}SqStack;
+
+int main(void)
+{
+
+
+	return 0;
+}
