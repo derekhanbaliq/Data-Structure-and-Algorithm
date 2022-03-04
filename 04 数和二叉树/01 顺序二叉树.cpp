04216d27ce7def5b6a765fc384ffc7d997a659d1
Derek Zhou <derekzhou1105@gmail.com>
2022-03-04 18:46:59 -0500

    SqBiTree

---
+++ 04 数和二叉树/01 顺序二叉树.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+#define TRUE			1
+#define FALSE			0
+
+#define OK				1
+#define ERROR			0
+#define OVERFLOW		-1
+#define UNDERFLOW		-2
+
+#define MAXSIZE			100
+
+typedef int TElemType;
+typedef TElemType SqBiTree[MAXSIZE];
+
+int main(void)
+{
+	SqBiTree bt;
+
+
+
+	return 0;
+}
