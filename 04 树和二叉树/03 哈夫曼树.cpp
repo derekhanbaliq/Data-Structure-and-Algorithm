05e2e26dcfdba4f9cd76ba44e6008718856139c6
Derek Zhou <derekzhou1105@gmail.com>
2022-03-19 13:31:56 -0400

    Huffman code

--- 04 树和二叉树/03 哈夫曼树.cpp
+++ 04 树和二叉树/03 哈夫曼树.cpp
@@ -19,6 +19,8 @@ typedef struct
 	int parent, lch, rch;
 }HTNode, * HuffmanTree;
 
+typedef char* HuffmanCode;
+	
 //构造哈夫曼树--哈夫曼算法
 void CreateHuffmanTree(HuffmanTree HT, int n)
 {
@@ -66,6 +68,46 @@ void CreateHuffmanTree(HuffmanTree HT, int n)
 void Select(HuffmanTree HT, int n, int s1, int s2)
 {
 	;
+}
+
+//存疑?!?!
+//从叶子到根逆向求每个字符的哈夫曼编码，存储在编码表HC中
+void CreateHuffmanCode(HuffmanTree HT, HuffmanCode* & HC, int n)
+{
+	HC = new HuffmanCode[n + 1]; //分配n个字符编码的头指针矢量
+	char* cd = new char[n]; //分配临时存放编码的动态数组空间
+
+	cd[n - 1] = '\0'; //编码结束符
+
+	for (int i = 1; i <= n; ++i) //逐个字符求哈夫曼编码
+	{
+		int start = i - 1;
+		int c = i; 
+		int f = HT[i].parent;
+
+		//从叶子结点开始向上回溯 直到根结点
+		while (f != 0)
+		{
+			--start; //回溯一次start向前指一个位置
+
+			if (HT[f].lch == c)
+			{
+				cd[start] = '0';
+			}
+			else
+			{
+				cd[start] = '1';
+			}
+
+			c = f;
+			f = HT[f].parent; //继续向上回溯 求出第i个字符的编码
+		}
+
+		HC[i] = new char[n - start]; //为第i个字符串编码分配空间
+		strcpy(HC[i], &cd[start]); //将求得的编码从临时空间cd复制到HC的当前行中
+	}
+
+	delete cd; //释放临时空间
 }
 
 int main(void)
