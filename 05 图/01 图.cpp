c16f6f3b27ac3e48936010535a78d4b4b2d2cd98
Derek Zhou <derekzhou1105@gmail.com>
2022-03-26 11:19:30 -0400

    graph

---
+++ 05 图/01 图.cpp
@@ -0,0 +1,12 @@
+#include "graph.h"
+
+using namespace std;
+
+
+
+int main(void)
+{
+	;
+
+	return 0;
+}
---
+++ 05 图/graph.h
@@ -0,0 +1,77 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+#define TRUE			1
+#define FALSE			0
+
+#define OK				1
+#define ERROR			0
+#define OVERFLOW		-1
+#define UNDERFLOW		-2
+
+typedef int Status;
+
+//邻接矩阵
+
+#define MaxInt 32767	//表示极大值 即infinity
+#define MVNum 100		//最大顶点数
+
+typedef char VerTexType;	//设顶点的数据类型为字符型
+typedef int ArcType;		//假设边的权值类型为整形
+
+typedef struct
+{
+	VerTexType vexs[MVNum];		//顶点表
+	ArcType arcs[MVNum][MVNum]; //邻接矩阵
+	int vexnum, arcnum;			//图的当前点数和边数
+}AMGraph; //Adjacency Matrix Graph
+
+
+//采用邻接矩阵表示法创建无向网
+
+Status CreateUDN(AMGraph& G)
+{
+	cin >> G.vexnum >> G.arcnum; //输入总顶点数 总边数
+
+	for (int i = 0; i < G.vexnum; ++i)
+	{
+		cin >> G.vexs[i]; //依次输入点的信息
+	}
+
+	for (int i = 0; i < G.vexnum; ++i) //依次输入点的信息
+	{
+		for (int j = 0; i < G.vexnum; ++j)
+		{
+			G.arcs[i][j] = MaxInt; //边的权值均置为极大值
+		}
+	}
+
+	VerTexType v1, v2;
+	int w;
+
+	for (int k = 0; k = G.arcnum; ++k) //构造邻接矩阵
+	{
+		cin >> v1 >> v2 >> w;
+
+		int i = LocateVex(G, v1);
+		int j = LocateVex(G, v2); //确定v1和v2在G中的位置
+
+		G.arcs[i][j] = w; //边<v1, v2>的权值置为w
+		G.arcs[i][j] = G.arcs[j][i]; //置<v1, v2>的对称边<v2, v1>的权值为w
+	}
+
+	return OK;
+}
+
+int LocateVex(AMGraph G, VerTexType i)
+{
+	int i = 0;
+
+	return i;
+}
+
+
+
+
