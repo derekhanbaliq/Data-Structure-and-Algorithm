b57bb114f66c132cd2412a8b0a7d64b2672c02cb
Derek Zhou <derekzhou1105@gmail.com>
2022-05-12 11:10:23 -0400

    binary find

--- 06 查找/01 顺序查找.cpp
+++ 06 查找/01 顺序查找.cpp
@@ -19,7 +19,7 @@ typedef int KeyType;
 typedef struct
 {
 	KeyType key; //关键字域
-	//其他域
+	//其他域...
 } ElemType;
 
 //顺序表结构类型定义
@@ -31,7 +31,69 @@ typedef struct
 
 SSTable ST; //定义顺序表ST
 
-int main(void)
+int Search_Seq(SSTable ST, KeyType key)
+{
+	for (int i = ST.length; i >= 1; --i)
+	{
+		if (ST.R[i].key == key)
+		{
+			return i;
+		}
+	}
+
+	return 0;
+}
+
+int Search_Seq_1(SSTable ST, KeyType key)
+{
+	int i;
+	
+	for (i = ST.length; ST.R[i].key != key; --i)
+	{
+		if (i <= 0)
+		{
+			break;
+		}
+	}
+
+	if (i > 0)
+	{
+		return i;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int Search_Seq_2(SSTable ST, KeyType key)
+{
+	int i;
+
+	for (i = ST.length; ST.R[i].key != key && i > 0; --i);
+
+	if (i > 0)
+	{
+		return i;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+//增加监视哨的顺序查找
+int Search_Seq_New(SSTable ST, KeyType key)
+{
+	int i;
+	
+	ST.R[0].key = key;
+
+	for (i = ST.length; ST.R[i].key != key && i > 0; --i);
+		return i;
+}
+
+int main01(void)
 {
 
 
---
+++ 06 查找/02 折半查找.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+using namespace std;
+
+//general define
+#define TRUE		1
+#define FALSE		0
+#define OK				1
+#define ERROR			0
+#define INFEASIBLE		-1
+#define STACK_OVERFLOW	-2
+
+typedef int Status;
+typedef int ElemType;
+typedef int KeyType;
+
+//数据元素类型定义
+typedef struct
+{
+	KeyType key; //关键字域
+	//其他域...
+} ElemType;
+
+//顺序表结构类型定义
+typedef struct
+{
+	ElemType* R; //表基地址
+	int length; //表长
+}SSTable; //Sequential Search Table
+
+SSTable ST; //定义顺序表ST
+
+int Search_Bin(SSTable ST, KeyType key)
+{
+	int low = 1;
+	int high = ST.length; //设置区间初值
+
+	while (low <= high)
+	{
+		int mid = (low + high) / 2;
+		
+		if (ST.R[mid].key == key)
+		{
+			return mid; //找到待查元素
+		}
+		else if (key < ST.R[mid].key) //缩小查找区间
+		{
+			high = mid - 1; //继续在前半区间进行查找
+		}
+		else
+		{
+			low = mid + 1; //继续在前半区间进行查找
+		}
+	}
+
+	return 0; //顺序表中不存在待查元素
+}
+
+//递归式折半查找
+int Search_Bin_Recursion(SSTable ST, KeyType key, int low, int high)
+{
+	if (low > high)
+	{
+		return 0; //查找不到时返回0
+	}
+
+	int mid = (low + high) / 2;
+
+	if (key == ST.R[mid].key)
+	{
+		return mid;
+	}
+	else if (key < ST.R[mid].key)
+	{
+		Search_Bin_Recursion(ST, key, low, mid - 1); //递归 在前半区间进行查找
+	}
+	else
+	{
+		Search_Bin_Recursion(ST, key, mid + 1, high); //递归 在后半区间进行查找
+	}
+}
+
+int main(void)
+{
+
+
+	return 0;
+}
