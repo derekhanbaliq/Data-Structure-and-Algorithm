f18678a960f8b35772e6d579a1652704742e5e80
Derek Zhou <derekzhou1105@gmail.com>
2022-05-21 13:30:54 -0400

    Shell

--- 07 排序/01 存储结构.cpp
+++ 07 排序/01 存储结构.cpp
@@ -30,6 +30,7 @@ typedef struct //定义顺序表的结构
 	int length; //顺序表的长度
 }SqList;
 
+//直接插入排序
 void InsertSort(SqList& L)
 {
 	int i, j;
@@ -44,6 +45,67 @@ void InsertSort(SqList& L)
 				L.r[j + 1] = L.r[j]; //记录后移
 			}
 			L.r[j + 1] = L.r[0]; //插入到正确位置
+		}
+	}
+}
+
+//折半插入排序
+void BInsertSort(SqList& L)
+{
+	int i, j, low, high, mid;
+
+	for (i = 2; i <= L.length; ++i)
+	{
+		L.r[0] = L.r[i]; //当前插入元素存到"哨兵"位置
+		low = 1;
+		high = i - 1; //采用二分查找法查找插入位置
+		
+		while (low <= high)
+		{
+			mid = (low + high) / 2;
+			if (L.r[0].key < L.r[mid].key)
+			{
+				high = mid - 1;
+			}
+			else
+			{
+				low = mid + 1;
+			}
+		} //循环结束 high + 1 为插入位置
+
+		for (j = i - 1; j >= high + 1; --j)
+		{
+			L.r[j + 1] = L.r[j]; //移动元素
+		}
+		L.r[high + 1] = L.r[0]; //插入到正确位置
+	}
+}
+
+//希尔排序算法(主程序)
+void ShellSort(SqList& L, int dlta[], int t)
+{
+	//按增量序列dlta[0...t-1]对顺序表L作希尔排序
+	for (int k = 0; k < t; ++k)
+	{
+		ShellInsert(L, dlta[k]); //一趟增量为dlta[k]的插入排序
+	}
+}
+
+//对顺序表L进行一趟增量为dk的Shell排序 dk为步长因子
+void ShellInsert(SqList& L, int dk)
+{
+	int i, j;
+	
+	for (i = dk + 1; i <= L.length; ++i)
+	{
+		if (L.r[i].key < L.r[i - dk].key)
+		{
+			L.r[0] = L.r[i];
+			for (j = i - dk; j > 0 && (L.r[0].key < L.r[j].key); j = j - dk)
+			{
+				L.r[j + dk] = L.r[j];
+			}
+			L.r[j + dk] = L.r[0];
 		}
 	}
 }
